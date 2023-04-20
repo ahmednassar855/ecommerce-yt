337f399d1ce250aeaeec8567ae010968f7666345
ahmednassar855 <ahmed.nassar855@gmail.com>
2023-04-20 05:31:48 +0200

    commit 3

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -7,11 +7,12 @@ const authRouter = express.Router()
 
 // routing
 authRouter.post('/register' , register)
- 
 authRouter.post('/login' , login)
-
+// protected routes
+authRouter.get('/user-auth' , requireSignIn , (req , res) => {
+    res.status(200).send({ ok : true })
+}) 
 // test route
-
 authRouter.get('/test' ,  requireSignIn , isAdmin ,testController)
 
 
